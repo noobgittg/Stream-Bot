600477e1512553db1af12e55541bdb76ecf6b0bb
Noob <akhilkrishnaus1234@gmail.com>
2025-09-20 14:47:13 +0530

    Update list.py

--- Code_X_Mania/bot/plugins/list.py
+++ Code_X_Mania/bot/plugins/list.py
@@ -2,12 +2,21 @@ from Code_X_Mania.bot import StreamBot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+LIST_MSG = """𝙃𝙞! {} ✨  
+𝙃𝙚𝙧𝙚’𝙨 𝙖 𝙡𝙞𝙨𝙩 𝙤𝙛 𝙖𝙡𝙡 𝙢𝙮 𝙘𝙤𝙢𝙢𝙖𝙣𝙙𝙨 ⬇️  
+
+1. `/start ⚡️`  
+2. `/help 📚`  
+5. `/ping 📡`  
+6. `/status 📊`  
+8. `/maintainers 😎`  
+"""
+
 @StreamBot.on_message(filters.command("list"))
-def _start(client, message):
-    client.send_message(chat_id = message.chat.id,
-        text = LIST_MSG.format(message.from_user.mention),
-        reply_to_message_id = message.message_id,
+def _list(client, message):
+    client.send_message(
+        chat_id=message.chat.id,
+        text=LIST_MSG.format(message.from_user.mention),
+        reply_to_message_id=message.message_id,
         parse_mode="markdown"
     )
-    
-    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `start⚡️` \n 2. `help📚` \n 3. `login🔑` \n 4.`follow❤️` \n 5. `ping📡` \n 6. `status📊` \n 7. `dc` this tells your telegram dc \n 8. `maintainers😎` "
