cd3b9bc2427d461bb104b2b7266b4025ffb60f44
Noob <akhilkrishnaus1234@gmail.com>
2025-10-08 22:14:28 +0530

    Update render_template.py

--- Code_X_Mania/utils/render_template.py
+++ Code_X_Mania/utils/render_template.py
@@ -18,32 +18,47 @@ async def fetch_properties(message_id):
         else f"{mimetypes.guess_type(file_name)}"
     return file_name, mime_type
 
-
 async def render_page(message_id):
     file_name, mime_type = await fetch_properties(message_id)
     src = urllib.parse.urljoin(Var.URL, str(message_id))
+
     audio_formats = [
-            'audio/mpeg', 'audio/mp4', 'audio/x-mpegurl', 'audio/vnd.wav',
-            'audio/ogg', 'audio/webm', 'audio/x-wav'
-        ]
-        video_formats = [
-            'video/mp4', 'video/avi', 'video/ogg', 'video/h264',
-            'video/h265', 'video/x-matroska', 'video/webm'
-        ]
+        'audio/mpeg', 'audio/mp4', 'audio/x-mpegurl',
+        'audio/vnd.wav', 'audio/ogg', 'audio/webm', 'audio/x-wav'
+    ]
+
+    video_formats = [
+        'video/mp4', 'video/avi', 'video/ogg', 'video/h264',
+        'video/h265', 'video/x-matroska', 'video/webm'
+    ]
+
+    # Default heading
+    heading = f"File: {file_name}"
+
+    # Handle video and audio
     if mime_type.lower() in video_formats:
-        async with aiofiles.open('Code_X_Mania/template/req.html') as r:
-            heading = 'Watch {}'.format(file_name)
-            tag = mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+        async with aiofiles.open('Code_X_Mania/template/req.html', mode='r') as r:
+            heading = f"Watch {file_name}"
+            tag = 'video'
+            html_template = await r.read()
+            html = html_template.replace('tag', tag) % (heading, file_name, src)
+
     elif mime_type.lower() in audio_formats:
-        async with aiofiles.open('Code_X_Mania/template/req.html') as r:
-            heading = 'Listen {}'.format(file_name)
-            tag = mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+        async with aiofiles.open('Code_X_Mania/template/req.html', mode='r') as r:
+            heading = f"Listen {file_name}"
+            tag = 'audio'
+            html_template = await r.read()
+            html = html_template.replace('tag', tag) % (heading, file_name, src)
+
+    # Handle other files (download link)
     else:
-        async with aiofiles.open('Code_X_Mania/template/dl.html') as r:
-            async with aiohttp.ClientSession() as s:
-                async with s.get(src) as u:
-                    file_size = human_size(u.headers.get('Content-Type'))
-                    html = (await r.read()) % (heading, file_name, src, file_size)
+        async with aiofiles.open('Code_X_Mania/template/dl.html', mode='r') as r:
+            async with aiohttp.ClientSession() as session:
+                async with session.head(src) as response:
+                    content_length = response.headers.get('Content-Length')
+                    file_size = human_size(int(content_length)) if content_length else "Unknown"
+            heading = f"Download {file_name}"
+            html_template = await r.read()
+            html = html_template % (heading, file_name, src, file_size)
+
     return html
